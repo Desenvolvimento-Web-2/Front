f605839cf812d16c3e46f50a931b5e75c61df4c5
Arthur Gian <89954777+arthurgian@users.noreply.github.com>
2023-08-21 15:05:52 -0300

    Correção de erros

--- src/app/perfil-funcionario/index.ts
+++ src/app/perfil-funcionario/index.ts
@@ -1,10 +1,3 @@
-export { VisualizacaoPedidoComponent } from './pedidos/visualizacao-pedido/visualizacao-pedido.component';
-export { PaginaInicialFuncionarioComponent } from './pedidos/pagina-inicial-funcionario/pagina-inicial-funcionario.component';
-export { ListarRoupaComponent } from './roupa/listar-roupa/listar-roupa.component';
-export { InserirEditarRoupaComponent } from './roupa/inserir-editar-roupa/inserir-editar-roupa.component';
 export { ListarFuncionarioComponent } from './funcionarios/listar-funcionario/listar-funcionario.component';
 export { InserirFuncionarioComponent } from './funcionarios/inserir-funcionario/inserir-funcionario.component';
 export { EditarFuncionarioComponent } from './funcionarios/editar-funcionario/editar-funcionario.component';
-export { RelatorioComponent } from './relatorios/relatorios-clientes/relatorios-clientes.component';
-export { RelatorioClientesFieisComponent } from './relatorios/relatorios-clientes-fieis/relatorios-clientes-fieis.component';
-export { RelatorioReceitasComponent } from './relatorios/relatorios-receitas/relatorios-receitas.component';
